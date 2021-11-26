d7ff8c41ae4368d98bbdf84d229935f38cb4b647
RosieRuby <ngoccodes@gmail.com>
2021-11-25 23:02:40 -0600

    guianh
    guianh

---
+++ main.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    $("#one").on("click", function () {
+        alert("She's thankful for this year because she met you. You brought her joy, happiness and you taught her much about love and what how it feels to be in love with someone.");
+    });
+
+    $("#two").on("click", function () {
+        alert("She's truely happy and thankful for all the moments sad and happy, confused and clear, boring and peaceful that you guys have spent with each other.");
+    });
+
+    $("#three").on("click", function () {
+        alert("She feels so lucky to have you as her first boyfriend and her first love because you have a good heart, patient and loving.");
+    });
+
+    $("#lastly").on("click", function () {
+        alert("She wants to let you know: \"It's cheesy and I have heard a lot of advice not to say this in times like this or you will get tired of me. However, I think it is exactly this time when I need to let you know that I always have you in my heart, love you, and believe in our love so, bb, have your heart strong, your mind peaceful and smile when the next day comes. Your smile is one of the cutest and brightest things I have ever witnessed. -kiss you --Tho\"");
+    });
+
+});
+
